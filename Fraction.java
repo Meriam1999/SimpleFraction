60da3d64200c510d1a2e6f5ae9cb4dc0ba072fcb
Souissi Meriam <souissi1mariem@gmail.com>
2021-10-01 12:15:58 +0200

    commit

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,18 @@
 class Fraction {
-    public static void main (String[] args)
-    {
-        System.out.println("hellol"); 
+    int a ; 
+    int b ; 
+   
+    public Fraction (int a , int b ) 
+    { 
+        this.a=a ; 
+        this.b=b; 
+        assert a!=0;
+    
     }
-}
+    public String toString() 
+    {
+        return Integer.toString (this.a)+"/"+ Integer.toString(this.b) ; 
+    }
+
+    }
+     
--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main
 {
 public static void main (String[] args)
     {
-        System.out.println("Bonjour"); 
+        Fraction f1 = new Fraction(2,3) ; 
+        System.out.println(f1.toString()) ; 
+
     }
 }
