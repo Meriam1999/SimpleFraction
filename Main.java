d31b72ff4de8834b22f6a8f6db3596110647213d
Souissi Meriam <souissi1mariem@gmail.com>
2021-10-01 13:26:53 +0200

    okl

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public static void main (String[] args)
     {
         Fraction f1 = new Fraction(2,3) ; 
         System.out.println(f1.toString()) ; 
+        System.out.println("hey");
 
     }
 }
